41577d175a2977f8356b41dfbf649c0276528197
eldhose <eldho1998@gmail.com>
2021-11-06 18:07:19 +0530

    password strength added.

--- js/validatesignup.js
+++ js/validatesignup.js
@@ -7,6 +7,7 @@ var nameerror = document.getElementById("nameerror");
 let emailRegex = /^([A-Za-z0-9\.-]+)\@([A-Za-z0-9-]+)\.([A-Za-z]{2,3})(\.[A-Za-z]{2,3})?$/;
 let mobileRegex = /^[0-9]{10,10}$/;
 let nameRegex = /^([A-Za-z]{1,30})(\s[A-Za-z]{1,30}){0,2}$/;
+let spChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 var pwd = document.getElementById("pwd");
 var pwderror = document.getElementById("pwderror");
 var formsubmitsignup = document.getElementById("formsubmitsignup");
@@ -65,32 +66,65 @@ function namevalidate() {
 
 function pwdvalidate() {
     p1 = p2 = p3 = p4 = 0;
+    var pstrength = 0;
     p4w = "<span class='text-danger'>✖ Minimum 8 characters required</span>";
     p3w = "<span class='text-danger'>✖ Atleast one number required</span>";
     p2w = "<span class='text-danger'>✖ Atleast one lower case required</span>";
     p1w = "<span class='text-danger'>✖ Atleast one upper case required</span>";
     if (pwd.value.length >= 8) {
         p4 = 1; // length condition good
+        pstrength = pstrength + 1;
         p4w = "<span class='text-success'>✔ Minimum 8 characters found</span>";
     }
     if (p3r.test(pwd.value)) {
         p3 = 1; // at least one number condition good
+        pstrength = pstrength + 1;
         p3w = "<span class='text-success'>✔ Atleast one number found</span>";
     }
     if (p2r.test(pwd.value)) {
         p2 = 1; // one lowercase condition good
+        pstrength = pstrength + 1;
         p2w = "<span class='text-success'>✔ Atleast one lower case found</span>";
     }
     if (p1r.test(pwd.value)) {
         p1 = 1; // one uppercase condition good
+        pstrength = pstrength + 1;
         p1w = "<span class='text-success'>✔ Atleast one upper case found</span>";
+    }
+    if (spChars.test(pwd.value)) {
+        pstrength = pstrength + 1;
+    }
+
+    if (pstrength <= 0) {
+        pstrengthbg = 'bg-danger';
+        pl = 5;
+    }
+    if (pstrength == 1) {
+        pstrengthbg = 'bg-danger';
+        pl = 20;
+    }
+    if (pstrength == 2) {
+        pstrengthbg = 'bg-warning';
+        pl = 40;
+    }
+    if (pstrength == 3) {
+        pstrengthbg = 'bg-info';
+        pl = 60;
+    }
+    if (pstrength == 4) {
+        pstrengthbg = 'bg-success';
+        pl = 80;
+    }
+    if (pstrength >= 5) {
+        pstrengthbg = 'bg-success';
+        pl = 100;
     }
 
     if (p1 == 1 && p2 == 1 && p3 == 1 && p4 == 1) {
-        pwderror.innerHTML = "<div class='mt-4 alert alert-success rounded-0'>✔ Password looks fine.</div>";
+        pwderror.innerHTML = "<div class='mt-4 alert alert-success rounded-0'>✔ Password looks fine.</br></br>Password Strength: <div class='progress'><div class='progress-bar " + pstrengthbg + "' role='progressbar' style='width: " + pl + "%' aria-valuenow=" + pstrength + " aria-valuemin='0' aria-valuemax='5'></div></div></div></div>";
         pwdvalid = 1;
     } else {
-        pwderror.innerHTML = "<div class='mt-4 alert alert-info rounded-0'>" + p4w + "</br>" + p3w + "</br>" + p2w + "</br>" + p1w + "</div>";
+        pwderror.innerHTML = "<div class='mt-4 alert alert-info rounded-0'>" + p4w + "</br>" + p3w + "</br>" + p2w + "</br>" + p1w + "</br></br>Password Strength: <div class='progress'><div class='progress-bar " + pstrengthbg + "' role='progressbar' style='width: " + pl + "%' aria-valuenow=" + pstrength + " aria-valuemin='0' aria-valuemax='5'></div></div></div></div>";
         pwdvalid = 0;
     }
 }
